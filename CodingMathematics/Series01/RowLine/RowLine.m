910e360108e009882563986d0a62f83f274db7ae
Alec K <boltakziy@gmail.com>
2025-06-25 22:52:28 +0900

    Series 1

---
+++ CodingMathematics/Series01/RowLine/RowLine.m
@@ -0,0 +1,16 @@
+clear;
+clf;
+% Drawing initialization
+
+nx = 16;
+ny = 36;
+
+for i = 1 : ny
+    % til not bigger than 'ny'
+    line([0 nx], [i i])
+    % add rows.
+
+end
+
+axis image
+% Screen ratio adjustment
