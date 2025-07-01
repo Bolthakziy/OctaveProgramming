ad24ceae37fe91183cfd7ff57170d45ad9c942bb
Alec K <boltakziy@gmail.com>
2025-07-01 21:44:47 +0900

    Series 1

---
+++ CodingMathematics/Series01/TestPlot/TestPlot.m
@@ -0,0 +1,9 @@
+clear;
+clf;
+
+x = [-7, -6, -5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5, 6, 7];
+y = [7, 6, 5, 4, 3, 2, 1, 0, 1, 2, 3, 4, 5, 6, 7];
+% The number of 'x' and the number of 'y' should be same!
+
+plot(x, y, "-b", "linewidth", 3)
+% Display a graph. In it, there is a blue line with a 3-width.
